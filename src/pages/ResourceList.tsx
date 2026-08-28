28869d17939d02fa1b44bcdbce238cb23e060468
Tommy Hulten <tommy.hulten@gmail.com>
2026-08-28 14:33:57 +0200

    Pages

---
+++ src/pages/ResourceList.tsx
@@ -0,0 +1,15 @@
+
+
+export default function ResourceList() {
+  return (
+    <div>
+      <h1>ResourceList</h1>
+    </div>
+  );
+}
+
+ResourceList.route = {
+  path: "/resourses",
+  order: 2,
+  label: "Se rum",
+};
