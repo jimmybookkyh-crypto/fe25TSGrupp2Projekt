55a2be5e5fb31f06d40c6f5e7bbba79ca5830595
Tommy Hulten <tommy.hulten@gmail.com>
2026-08-31 14:09:57 +0200

    Deatails

--- src/pages/ResourceDetails.tsx
+++ src/pages/ResourceDetails.tsx
@@ -1,6 +1,17 @@
+import { useState } from "react";
+import { useParams, useSearchParams } from "react-router";
+import useFetch from "../utils/useFetch";
+import { allSlots, type Room, type Booking} from "../interfaces/types"
 
 
 export default function ResourceDetails() {
+  const { id } = useParams();
+  const [searchParams] = useSearchParams();
+  const date = searchParams.get("date");
+  const [selectedSlots, setSelectedSlots] = useState<string[]>([]);
+ 
+ 
+ 
   return (
     <div>
       <h1>ResourceDetails</h1>
@@ -9,7 +20,7 @@ export default function ResourceDetails() {
 }
 
 ResourceDetails.route = {
-  path: "/Resources/:id",
+  path: "/resources/:id",
   order: 3,
   label: "Rum detaljer",
 };
