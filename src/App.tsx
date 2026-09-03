723916381a8c5808b9743f273b3c0eede40eee42
WilliamEngman24 <williamengman24@gmail.com>
2026-09-03 11:41:32 +0200

    base structure for header and footer in partials folder. implemented in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import { Outlet } from "react-router";
 
+import Header from "./partials/Header.tsx"
+import Footer from "./partials/Footer.tsx"
+
 export default function App() {
   return <>
+    <Header />
     <main>
       <Outlet />
     </main>
+    <Footer />
   </>;
 }
